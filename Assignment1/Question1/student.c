5d50737f3425f629392ef4c5670e21a96d6e6ea7
Marco Lopez <marco.luis.lopez.14@gmail.com>
2026-02-20 12:08:41 -0600

    Finish Assignment 1

--- Assignment1/Question1/student.c
+++ Assignment1/Question1/student.c
@@ -1,22 +1,27 @@
 #include "Student.h"
 
-//You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n:
-//nums1 has length m + n
-//The first m values in nums1 are real elements
-//The last n values in nums1 are placeholders (0)
-//nums2 has length n
-//Goal: Merge nums2 into nums1 so that nums1 becomes one sorted array.
-//Return: Nothing (modify nums1 in-place).
-
-//Example:
-//Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
-//Output: nums1 = [1,2,2,3,5,6]
-//Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
-
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int a = m - 1;
+    int b = n - 1;
+    int pos = m + n - 1;
 
- // TODO: implement
+    while (a >= 0 && b >= 0) {
+        if (nums1[a] > nums2[b]) {
+            nums1[pos] = nums1[a];
+            a--;
+        } else {
+            nums1[pos] = nums2[b];
+            b--;
+        }
+        pos--;
+    }
 
+    while (b >= 0) {
+        nums1[pos] = nums2[b];
+        b--;
+        pos--;
+    }
 
-
+    (void)nums1Size;
+    (void)nums2Size;
 }
--- Assignment1/Question3/student.c
+++ Assignment1/Question3/student.c
@@ -1,33 +1,39 @@
 #include "Student.h"
 #include <stdlib.h>
 
-//You are given a large integer represented as an integer array digits, where:
-
-//digits[i] is the i-th digit of the integer
-//digits are ordered from most significant to least significant
-//there are no leading 0s (except the number 0 itself in some of our tests)
-//Goal: Add 1 to the number and return the resulting digits array.
-
-//Example 1:
-//Input: digits = [4,3,2,1]
-//Output: [4,3,2,2]
-//Explanation: The array represents the integer 4321.
-//Incrementing by one gives 4321 + 1 = 4322.
-//Thus, the result should be [4,3,2,2].
-
-//Example 2:
-//Input: digits = [9]
-//Output: [1,0]
-//Explanation: The array represents the integer 9.
-//Incrementing by one gives 9 + 1 = 10.
-//Thus, the result should be [1,0].
-
-//Return value (int*): the output digits array.
-//Output parameter (returnSize): set *returnSize to the number of digits in the returned array.
-
 int* plusOne(int* digits, int digitsSize, int* returnSize) {
-    // TODO: implement
+    int carry = 1;
 
-    
+    int* result = (int*)malloc(sizeof(int) * digitsSize);
+
+    for (int i = 0; i < digitsSize; i++) {
+        result[i] = digits[i];
+    }
+
+    for (int i = digitsSize - 1; i >= 0; i--) {
+        if (result[i] + carry < 10) {
+            result[i] += carry;
+            carry = 0;
+            break;
+        } else {
+            result[i] = 0;
+            carry = 1;
+        }
+    }
+
+    if (carry == 1) {
+        int* newResult = (int*)malloc(sizeof(int) * (digitsSize + 1));
+
+        newResult[0] = 1;
+        for (int i = 0; i < digitsSize; i++) {
+            newResult[i + 1] = 0;
+        }
+
+        *returnSize = digitsSize + 1;
+        free(result);
+        return newResult;
+    }
+
+    *returnSize = digitsSize;
+    return result;
 }
-
